3c470a78b378ddfbb41c60142c012eae39d20202
51133131 <51133131@nfu.edu.tw>
2023-03-31 11:43:02 +0800

    0.0

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': '666 \n \n About \n hi \n', 'tags': '', 'url': 'About.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': '666 \n \n \n', 'tags': '', 'url': 'About.html'}]};
